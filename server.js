a178b4a3c2e5bbf9cdc4d71bb5dafd6cd69b6761
lamsmallari <lamsmallarijr@gmail.com>
2024-04-11 21:18:45 +0800

    install errorhandler

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const errorHandler = require('express-error-handler');
 const cors = require('cors');
 
 const PORT = process.env.PORT || 4001;
@@ -8,6 +9,8 @@ const PORT = process.env.PORT || 4001;
 app.use(cors());
 
 app.use(bodyParser.json());
+
+app.use(errorHandler());
 
 app.listen(PORT, () => {
   console.log(`Server is listening on ${PORT}`);
